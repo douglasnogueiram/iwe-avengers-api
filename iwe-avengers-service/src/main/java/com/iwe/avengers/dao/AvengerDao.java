3069c60eeab19eaf63c495bf7ac85861b18f341e
Admin <Admin@DESKTOP-LK0OECV>
2018-09-24 19:34:18 -0300

    Ajuste id

--- iwe-avengers-service/src/main/java/com/iwe/avengers/dao/AvengerDao.java
+++ iwe-avengers-service/src/main/java/com/iwe/avengers/dao/AvengerDao.java
@@ -12,7 +12,7 @@ public class AvengerDao {
 	
 	public AvengerDao() {
 		
-		mapper.put("ffff-gggg-hhhh-iiii", new Avenger("aaaa-bbbb-cccc-dddd", "Captain America", "Steve Rogers"));
+		mapper.put("ffff-gggg-hhhh-iiii", new Avenger("ffff-gggg-hhhh-iiii", "Captain America", "Steve Rogers"));
 		
 		mapper.put("aaaa-bbbb-cccc-dddd", new Avenger("aaaa-bbbb-cccc-dddd", "Hulk", "Bruce Banner"));
 		
